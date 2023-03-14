76a8f92c2981867f3c8855deff9a6b7893f7d549
cin-namon-dy-s <120317466+cinnamondys@users.noreply.github.com>
2023-03-14 18:56:14 +0700

    Update profile_detail_screen.dart

--- M02 - Layout/profile/profile_detail_screen.dart
+++ M02 - Layout/profile/profile_detail_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:case_study_latihan/custom.dart';
 
 class ProfileDetailScreen extends StatelessWidget {
   const ProfileDetailScreen({Key? key}) : super(key: key);
@@ -7,12 +8,12 @@ class ProfileDetailScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Profile Detail'),
+        title: const Text('Profile Detail (211110347 - Cindy Sintiya)'),
       ),
       body: Center(
         child: Column(
           children: [
-            const Text('Ini adalah Detail Profile'),
+            const Text('Ini adalah Detail Profile', style: TextStyle(fontSize: 22),),
             Wrap(
               // mainAxisAlignment: MainAxisAlignment.center,
               // crossAxisAlignment: CrossAxisAlignment.start,
@@ -29,21 +30,21 @@ class ProfileDetailScreen extends StatelessWidget {
                         border: Border.all(color: Colors.black),
                         color: Colors.blue[50],
                       ),
-                      child: Text('Strawberry Pavlova', textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold),),
+                      child: Text('Strawberry Pavlova', textAlign: TextAlign.center, style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18),),
                     ),
                     Container(
                       margin: const EdgeInsets.all(5.0),
-                      padding: const EdgeInsets.all(5.0),
+                      padding: const EdgeInsets.all(3.0),
                       width: 220,
                       decoration: BoxDecoration(
                         border: Border.all(color: Colors.black),
                         color: Colors.blue[50],
                       ),
-                      child: Text('Pavlova is a meringue-based dessert named after the Russian ballerine Anna Pavlova.\nPavlova featues a crisp crust and soft, light inside, topped with fruit and whipped cream.', softWrap: true, textAlign: TextAlign.center,),
+                      child: Text('Pavlova is a meringue-based dessert named after the Russian ballerine Anna Pavlova.\nPavlova featues a crisp crust and soft, light inside, topped with fruit and whipped cream.', textAlign: TextAlign.center, softWrap: true,),
                     ),
                     Container(
                       margin: const EdgeInsets.all(5.0),
-                      padding: const EdgeInsets.all(5.0),
+                      padding: const EdgeInsets.all(3.0),
                       width: 220,
                       decoration: BoxDecoration(
                         border: Border.all(color: Colors.black),
@@ -54,11 +55,11 @@ class ProfileDetailScreen extends StatelessWidget {
                         children: [
                           Row(
                             children: [
-                              Icon(Icons.star),
-                              Icon(Icons.star),
-                              Icon(Icons.star),
-                              Icon(Icons.star),
-                              Icon(Icons.star),
+                              Icon(Icons.star, size: 18,),
+                              Icon(Icons.star, size: 18,),
+                              Icon(Icons.star, size: 18,),
+                              Icon(Icons.star, size: 18,),
+                              Icon(Icons.star, size: 18,),
                             ],
                           ),
                           Text('170 Reviews'),
@@ -67,7 +68,7 @@ class ProfileDetailScreen extends StatelessWidget {
                     ),
                     Container(
                       margin: const EdgeInsets.all(5.0),
-                      padding: const EdgeInsets.all(5.0),
+                      padding: const EdgeInsets.all(3.0),
                       width: 220,
                       decoration: BoxDecoration(
                         border: Border.all(color: Colors.black),
@@ -78,23 +79,32 @@ class ProfileDetailScreen extends StatelessWidget {
                         children: [
                           Column(
                             children: [
-                              Icon(Icons.book_outlined, color: Colors.lightGreen,),
-                              Text('PREP :'),
-                              Text('25 min'),
+                              Icon(Icons.kitchen_outlined, color: Colors.lightGreen, size: 20,),
+                              Container(
+                                margin: EdgeInsets.only(top: 5, bottom: 8),
+                                child: Text('PREP:', style: TextStyle(fontSize: 12),),
+                              ),
+                              Text('25 min', style: TextStyle(fontSize: 12),),
                             ],
                           ),
                           Column(
                             children: [
-                              Icon(Icons.timer_outlined, color: Colors.lightGreen,),
-                              Text('COOK :'),
-                              Text('1 hr'),
+                              Icon(Icons.timer_outlined, color: Colors.lightGreen, size: 20,),
+                              Container(
+                                margin: EdgeInsets.only(top: 5, bottom: 8),
+                                child: Text('COOK:', style: TextStyle(fontSize: 12),),
+                              ),
+                              Text('1 hr', style: TextStyle(fontSize: 12),),
                             ],
                           ),
                           Column(
                             children: [
-                              Icon(Icons.food_bank_outlined, color: Colors.lightGreen,),
-                              Text('FEEDS :'),
-                              Text('4-6'),
+                              Icon(Icons.restaurant, color: Colors.lightGreen, size: 20,),
+                              Container(
+                                margin: EdgeInsets.only(top: 5, bottom: 8),
+                                child: Text('FEEDS:', style: TextStyle(fontSize: 12),),
+                              ),
+                              Text('4-6', style: TextStyle(fontSize: 12),),
                             ],
                           ),
                         ],
@@ -108,11 +118,23 @@ class ProfileDetailScreen extends StatelessWidget {
                 )
               ],
             ),
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              }, 
-              child: const Text('Kembali')
+            Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  }, 
+                  child: const Text('Kembali')
+                ),
+                SizedBox(height: 15,),
+                ElevatedButton(
+                  onPressed: () {
+                    Navigator.push(context, MaterialPageRoute(builder: (context) => const CustomScreen()));
+                  }, 
+                  child: const Text('Lihat Custom GridView Builder')
+                ),
+              ],
             )
           ]
         ),
